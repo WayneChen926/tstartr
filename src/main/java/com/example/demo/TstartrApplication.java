d64c34e6c5ff61f7156065fe758c216c69d4c031
WayneChen0926 <o9000889@gmail.com>
2021-04-19 13:45:18 +0800

    增加 check active 確認目前打包環境

--- src/main/java/com/example/demo/TstartrApplication.java
+++ src/main/java/com/example/demo/TstartrApplication.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
 public class TstartrApplication extends SpringBootServletInitializer {
+    
+    @Value("${spring.profiles.active}")
+    private String active;
     
     // 外部tomcat 佈署設定檔
     @Override
@@ -18,5 +26,16 @@ public class TstartrApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TstartrApplication.class, args);
+    }
+    
+    @RestController
+    class checkHealth{
+        
+        @GetMapping
+        public Map<String,String> check(){
+            Map<String,String> m = new HashMap<>();
+            m.put("check active", active);
+            return m ;
+        }
     }
 }
